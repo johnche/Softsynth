57b4f2e4459f449f27437171605c6e3f3255bea4
johnche <john20chen@gmail.com>
2019-10-30 22:31:19 +0100

    use pitch lookup table and better handling of keyboardplay

--- rtsynth/main.py
+++ rtsynth/main.py
@@ -1,6 +1,6 @@
 from synth import Synthesizer
 from pynput import keyboard
-import select, socket
+import select, socket, json
 
 
 class SynthController:
@@ -14,6 +14,12 @@ class SynthController:
 		#print('TCP server listening to %s %s' % self.address)
 		print('Setting up synth')
 		self.synth = Synthesizer()
+		with open('pitches.json', 'r') as f:
+			self.lookup_json = json.load(f)
+		self.pitches = [self.lookup_json[octave] for octave in self.lookup_json]
+
+		self.current_octave = 4
+		self.pressed_keys = []
 
 	def run_socket(self):
 		print('Ready')
@@ -37,46 +43,51 @@ class SynthController:
 		print('Exiting.')
 		self.close()
 
+	def register_key(self, key):
+		if key not in self.pressed_keys:
+			self.pressed_keys.append(key)
+
 	def on_press(self, key):
 		try:
-			if key.char == 'a':
-				self.synth.set_frequency(262)
-			elif key.char == 's':
-				self.synth.set_frequency(294)
-			elif key.char == 'd':
-				self.synth.set_frequency(330)
-			elif key.char == 'f':
-				self.synth.set_frequency(349)
-			elif key.char == 'g':
-				self.synth.set_frequency(392)
-			elif key.char == 'h':
-				self.synth.set_frequency(440)
-			elif key.char == 'j':
-				self.synth.set_frequency(494)
+			pitch_name = ''
+			if key.char == 'a': pitch_name = 'C'
+			elif key.char == 'w': pitch_name = 'C#'
+			elif key.char == 's': pitch_name = 'D'
+			elif key.char == 'e': pitch_name = 'D#'
+			elif key.char == 'd': pitch_name = 'E'
+			elif key.char == 'f': pitch_name = 'F'
+			elif key.char == 't': pitch_name = 'F#'
+			elif key.char == 'g': pitch_name = 'G'
+			elif key.char == 'y': pitch_name = 'G#'
+			elif key.char == 'h': pitch_name = 'A'
+			elif key.char == 'u': pitch_name = 'A#'
+			elif key.char == 'j': pitch_name = 'B'
 			elif key.char == 'k':
-				self.synth.set_frequency(523)
+				self.synth.set_frequency(self.pitches[self.current_octave+1]['C'])
 
-			elif key.char == 'w':
-				self.synth.set_frequency(278)
-			elif key.char == 'e':
-				self.synth.set_frequency(311)
-			elif key.char == 't':
-				self.synth.set_frequency(370)
-			elif key.char == 'y':
-				self.synth.set_frequency(415)
-			elif key.char == 'u':
-				self.synth.set_frequency(466)
+			if pitch_name:
+				self.synth.set_frequency(self.pitches[self.current_octave][pitch_name])
+
+			self.register_key(key.char)
 		except AttributeError:
 			if key == keyboard.Key.up:
-				self.synth.set_frequency(self.synth.frequency + 1)
+				if self.current_octave < 8:
+					self.current_octave += 1
 			elif key == keyboard.Key.down:
-				self.synth.set_frequency(self.synth.frequency - 1)
+				if self.current_octave > 0:
+					self.current_octave -= 1
 	
 	def on_release(self, key):
 		if key == keyboard.Key.esc:
+			self.close()
 			return False
 		else:
-			self.synth.set_frequency(0)
+			try:
+				self.pressed_keys.remove(key.char)
+				if not self.pressed_keys:
+					self.synth.set_frequency(0)
+			except AttributeError:
+				pass
 
 	def run_keyboard(self):
 		listener = keyboard.Listener(
