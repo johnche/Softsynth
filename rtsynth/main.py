e3ba9b803a925531c9ad2760eafdab1ce2c6558e
johnche <john20chen@gmail.com>
2020-01-11 11:08:15 +0100

    update controller/main and refactor vco

--- rtsynth/main.py
+++ rtsynth/main.py
@@ -1,10 +1,13 @@
+import select, socket
 from synth import Synthesizer
 from pynput import keyboard
-import select, socket, json
+from pitches import TABLE_PITCHES 
+from oscillators import SineOscillator
+from filters import LowpassFilter
 
 
 class SynthController:
-	def __init__(self, port):
+	def __init__(self, port, synthesizer):
 		#self.address = ("0.0.0.0", port)
 		#self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		#self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -13,13 +16,21 @@ class SynthController:
 		#self.sock.listen(1)
 		#print('TCP server listening to %s %s' % self.address)
 		print('Setting up synth')
-		self.synth = Synthesizer()
-		with open('pitches.json', 'r') as f:
-			self.lookup_json = json.load(f)
+		self.synth = synthesizer
+		self.lookup_json = TABLE_PITCHES
 		self.pitches = [self.lookup_json[octave] for octave in self.lookup_json]
 
 		self.current_octave = 4
 		self.pressed_keys = []
+		self.midi_num = 50
+		self.lp_c = 1000
+		self.lp = LowpassFilter()
+
+		self.synth += SineOscillator
+		self.synth += self.lp
+		self.synth.set_lowpass(self.lp_c)
+		self.synth.update()
+		self.synth.start()
 
 	def run_socket(self):
 		print('Ready')
@@ -49,53 +60,78 @@ class SynthController:
 
 	def on_press(self, key):
 		try:
-			pitch_name = ''
-			if key.char == 'a': pitch_name = 'C'
-			elif key.char == 'w': pitch_name = 'C#'
-			elif key.char == 's': pitch_name = 'D'
-			elif key.char == 'e': pitch_name = 'D#'
-			elif key.char == 'd': pitch_name = 'E'
-			elif key.char == 'f': pitch_name = 'F'
-			elif key.char == 't': pitch_name = 'F#'
-			elif key.char == 'g': pitch_name = 'G'
-			elif key.char == 'y': pitch_name = 'G#'
-			elif key.char == 'h': pitch_name = 'A'
-			elif key.char == 'u': pitch_name = 'A#'
-			elif key.char == 'j': pitch_name = 'B'
-			elif key.char == 'k':
-				self.synth.set_frequency(self.pitches[self.current_octave+1]['C'])
+			#pitch_name = ''
+			#if key.char == 'a': pitch_name = 'C'
+			#elif key.char == 'w': pitch_name = 'C#'
+			#elif key.char == 's': pitch_name = 'D'
+			#elif key.char == 'e': pitch_name = 'D#'
+			#elif key.char == 'd': pitch_name = 'E'
+			#elif key.char == 'f': pitch_name = 'F'
+			#elif key.char == 't': pitch_name = 'F#'
+			#elif key.char == 'g': pitch_name = 'G'
+			#elif key.char == 'y': pitch_name = 'G#'
+			#elif key.char == 'h': pitch_name = 'A'
+			#elif key.char == 'u': pitch_name = 'A#'
+			#elif key.char == 'j': pitch_name = 'B'
+			#elif key.char == 'k':
+			#	self.synth.set_frequency(self.pitches[self.current_octave+1]['C'])
 
-			if pitch_name:
-				self.synth.set_frequency(self.pitches[self.current_octave][pitch_name])
+			#if pitch_name:
+			#	self.synth.set_frequency(self.pitches[self.current_octave][pitch_name])
+
+			if key.char == 'a':
+				self.synth.play(self.midi_num)
 
 			self.register_key(key.char)
 		except AttributeError:
 			if key == keyboard.Key.up:
-				if self.current_octave < 8:
-					self.current_octave += 1
+				if self.midi_num < 128:
+					self.midi_num += 1
+					self.synth.play(self.midi_num)
+					print('Midi number', self.midi_num)
 			elif key == keyboard.Key.down:
-				if self.current_octave > 0:
-					self.current_octave -= 1
-
+				if self.midi_num >= 0:
+					self.midi_num -= 1
+					self.synth.play(self.midi_num)
+					print('Midi number', self.midi_num)
 			elif key == keyboard.Key.left:
-				if self.synth.oscillator_index > 0:
-					self.synth.set_oscillator(self.synth.oscillator_index - 1)
+				self.lp_c -= 1
+				print('Lowpass cutoff', self.lp_c)
+				self.synth.set_lowpass(self.lp_c)
+				self.synth.update()
 			elif key == keyboard.Key.right:
-				if self.synth.oscillator_index < 3:
-					self.synth.set_oscillator(self.synth.oscillator_index + 1)
+				self.lp_c += 1
+				print('Lowpass cutoff', self.lp_c)
+				self.synth.set_lowpass(self.lp_c)
+				self.synth.update()
 
-			print('Current octave', self.current_octave)
-			print('Current oscillator index', self.synth.oscillator_index)
+			#if key == keyboard.Key.up:
+			#	if self.current_octave < 8:
+			#		self.current_octave += 1
+			#elif key == keyboard.Key.down:
+			#	if self.current_octave > 0:
+			#		self.current_octave -= 1
+
+			#elif key == keyboard.Key.left:
+			#	if self.synth.oscillator_index > 0:
+			#		self.synth.set_oscillator(self.synth.oscillator_index - 1)
+			#elif key == keyboard.Key.right:
+			#	if self.synth.oscillator_index < 3:
+			#		self.synth.set_oscillator(self.synth.oscillator_index + 1)
+
+			#print('Current octave', self.current_octave)
+			#print('Current oscillator index', self.synth.oscillator_index)
 	
 	def on_release(self, key):
 		if key == keyboard.Key.esc:
-			self.close()
-			return False
+			pass
+			#self.close()
+			#return False
 		else:
 			try:
 				self.pressed_keys.remove(key.char)
 				if not self.pressed_keys:
-					self.synth.set_frequency(0)
+					self.synth.stop()
 			except AttributeError:
 				pass
 
@@ -105,16 +141,18 @@ class SynthController:
 				on_release=self.on_release)
 		listener.start()
 
-		self.synth.play()
-
 	def close(self):
-		self.conn.close()
-		self.sock.close()
-		self.synth.close()
+		#self.conn.close()
+		#self.sock.close()
+		#self.synth.close()
+		from sys import exit
+		exit()
 
 
 def main():
-	control = SynthController(5050)
+	synth = Synthesizer()
+
+	control = SynthController(5050, synth)
 	control.run_keyboard()
 	#control.run_socket()
 
